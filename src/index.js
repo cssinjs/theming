0b2be91ed58d0e8a01aafa1108aa85d44aa55de0
Vladimir Starkov <iamstarkov@gmail.com>
2017-04-25 02:02:06 +0200

    wip+

---
+++ src/index.js
@@ -0,0 +1,13 @@
+export { default as ThemeProvider } from './ThemeProvider';
+export { default as withTheme } from './withTheme';
+export { default as CHANNEL } from './channel';
+
+import ThemeProvider from './ThemeProvider';
+import withTheme from './withTheme';
+import CHANNEL from './channel';
+
+export default {
+  ThemeProvider,
+  withTheme,
+  CHANNEL,
+};
