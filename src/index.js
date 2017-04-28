0381b98b546c5093cb250939272f630f8e809256
Vladimir Starkov <iamstarkov@gmail.com>
2017-04-28 21:50:20 +0200

    wip themify

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const ThemeProvider = require('./ThemeProvider');
 const withTheme = require('./withTheme');
 const onThemeUpdate = require('./onThemeUpdate');
+const themify = require('./themify');
 const CHANNEL = require('./CHANNEL');
 
 module.exports = {
   ThemeProvider,
   withTheme,
   onThemeUpdate,
+  themify,
   CHANNEL,
 };
