239dc3f844cfe27c3ad038a946062daf15083f3c
Vladimir Starkov <iamstarkov@gmail.com>
2017-05-16 09:28:19 +0200

    finish with createWithTheme tests

--- src/create-with-theme.test.js
+++ src/create-with-theme.test.js
@@ -1,6 +1,5 @@
 import test from 'ava';
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { mount } from 'enzyme';
 import browserEnv from 'browser-env';
 
@@ -8,214 +7,171 @@ import isFunction from 'is-function';
 import createWithTheme from './create-with-theme';
 import channel from './channel';
 import createBroadcast from './create-broadcast';
-import { Comp, Pure } from './helpers';
+import {
+  getChannel,
+  Comp,
+  Pure,
+  ThemePropInterceptor,
+  mountOptions,
+  onTheme,
+} from './helpers';
 
 browserEnv(['window', 'document', 'navigator']);
 
 test(`createWithTheme's type`, t => {
-  t.true(isFunction(createWithTheme), `createWithTheme should be a function`);
+  const actual = isFunction(createWithTheme);
+  t.true(actual, `createWithTheme should be a function`);
 });
 
 test(`createWithTheme's result is function on its own`, t => {
   const withTheme = createWithTheme();
-  t.true(isFunction(withTheme), `withTheme should be a function`);
+  const actual = isFunction(withTheme);
+  t.true(actual, `withTheme should be a function`);
 });
 
 test(`withTheme(Comp) result instance type`, t => {
   const withTheme = createWithTheme();
-  t.true(
-    Component.isPrototypeOf(withTheme(Comp)),
-    `withTheme(Comp) should be a React Component`,
-  );
+  const actual = Component.isPrototypeOf(withTheme(Comp));
+  t.true(actual, `withTheme(Comp) should be a React Component`);
 });
-
-const getContextTypes = C => C.contextTypes;
-const getChannel = C => Object.keys(getContextTypes(C))[0];
 
 test(`withTheme(Comp)'s default channel`, t => {
   const withTheme = createWithTheme();
-  t.is(
-    getChannel(withTheme(Comp)),
-    channel,
-    `withTheme(Comp) should have default channel`,
-  );
+  const actual = getChannel(withTheme(Comp));
+  const expected = channel;
+  t.is(actual, expected, `withTheme(Comp) should have default channel`);
 });
 
 test(`withTheme(Comp) custom channel`, t => {
-  const withTheme = createWithTheme('__CUSTOM__');
-  t.is(
-    getChannel(withTheme(Comp)),
-    '__CUSTOM__',
-    `createWithTheme() should have custom channel`,
-  );
+  const custom = '__CUSTOM__';
+  const withTheme = createWithTheme(custom);
+  const actual = getChannel(withTheme(Comp));
+  const expected = custom;
+  t.is(actual, expected, `createWithTheme() should work with custom channel`);
 });
 
-class ThemePropInterceptor extends Component {
-  static propTypes = {
-    intercept: PropTypes.func.isRequired,
-    theme: PropTypes.object.isRequired,
-  };
-  constructor(props) {
-    super(props);
-    this.props.intercept(props.theme);
-  }
-  componentWillReceiveProps(nextProps) {
-    if (nextProps) {
-      this.props.intercept(nextProps.theme);
-    }
-  }
-  // eslint-disable-next-line
-  render() {
-    return <div />;
-  }
-}
-
-const mountOptions = broadcast => ({
-  childContextTypes: {
-    [channel]: PropTypes.func.isRequired,
-  },
-  context: {
-    [channel]: broadcast.subscribe,
-  },
-});
-
-test('withTheme(Comp) receive theme', t => {
+test(`withTheme(Comp) receive theme`, t => {
   const withTheme = createWithTheme();
-  const expectedTheme = { themed: true };
-  let actualTheme;
-  const updateActualTheme = theme => {
-    actualTheme = theme;
+  const theme = { themed: true };
+  let actual;
+  const updateActual = x => {
+    actual = x;
   };
+  const expected = theme;
 
   const ComponentWithTheme = withTheme(ThemePropInterceptor);
-  const broadcast = createBroadcast(expectedTheme);
+  const broadcast = createBroadcast(theme);
 
   mount(
-    <ComponentWithTheme intercept={updateActualTheme} />,
+    <ComponentWithTheme intercept={updateActual} />,
     mountOptions(broadcast),
   );
 
-  t.deepEqual(
-    actualTheme,
-    expectedTheme,
-    `withTheme(Comp) should receive theme`,
-  );
+  t.deepEqual(actual, expected, `withTheme(Comp) should receive theme`);
 });
 
 test(`withTheme(Comp) receive theme deep into tree`, t => {
   const withTheme = createWithTheme();
-  const expectedTheme = { themed: true };
-  let actualTheme;
-  const updateActualTheme = theme => {
-    actualTheme = theme;
+  const theme = { themed: true };
+  let actual;
+  const updateActual = x => {
+    actual = x;
   };
+  const expected = theme;
 
   const ComponentWithTheme = withTheme(ThemePropInterceptor);
-  const broadcast = createBroadcast(expectedTheme);
+  const broadcast = createBroadcast(expected);
 
   mount(
     <div>
       <div>
-        <ComponentWithTheme intercept={updateActualTheme} />
+        <ComponentWithTheme intercept={updateActual} />
       </div>
     </div>,
     mountOptions(broadcast),
   );
 
   t.deepEqual(
-    actualTheme,
-    expectedTheme,
+    actual,
+    expected,
     `withTheme(Comp) should receive a theme deep down into tree`,
   );
 });
 
 test(`withTheme(Comp) receives theme through PureComponent`, t => {
   const withTheme = createWithTheme();
-  const expectedTheme = { themed: true };
-  let actualTheme;
-  const updateActualTheme = theme => {
-    actualTheme = theme;
+  const theme = { themed: true };
+  let actual;
+  const updateActual = x => {
+    actual = x;
   };
+  const expected = theme;
 
   const ComponentWithTheme = withTheme(ThemePropInterceptor);
-  const broadcast = createBroadcast(expectedTheme);
+  const broadcast = createBroadcast(expected);
 
   mount(
     <Pure>
-      <ComponentWithTheme intercept={updateActualTheme} />
+      <ComponentWithTheme intercept={updateActual} />
     </Pure>,
     mountOptions(broadcast),
   );
 
   t.deepEqual(
-    actualTheme,
-    expectedTheme,
-    `withTheme(Comp) should receive a theme deep down into tree`,
+    actual,
+    expected,
+    `withTheme(Comp) should receive theme through PureComponent`,
   );
 });
 
 test(`withTheme(Comp) receives theme updates`, t => {
   const withTheme = createWithTheme();
-  const initTheme = { themed: true };
-  const updatedTheme = { updated: true };
-  let actualTheme;
-  const updateActualTheme = theme => {
-    actualTheme = theme;
+  const theme = { themed: true };
+  const update = { updated: true };
+  let actual;
+  const updateActualTheme = x => {
+    actual = x;
   };
+  const expected = update;
 
   const ComponentWithTheme = withTheme(ThemePropInterceptor);
-  const broadcast = createBroadcast(initTheme);
+  const broadcast = createBroadcast(theme);
 
   mount(
     <ComponentWithTheme intercept={updateActualTheme} />,
     mountOptions(broadcast),
   );
 
-  t.deepEqual(
-    actualTheme,
-    initTheme,
-    `withTheme(Comp) should receive initial theme`,
-  );
+  broadcast.publish(update);
 
-  broadcast.publish(updatedTheme);
-
-  t.deepEqual(
-    actualTheme,
-    updatedTheme,
-    `withTheme(Comp) should receive theme updates`,
-  );
+  t.deepEqual(actual, expected, `withTheme(Comp) should receive theme updates`);
 });
 
 test(`withTheme(Comp) receives theme updates even through PureComponent`, t => {
   const withTheme = createWithTheme();
-  const initTheme = { themed: true };
-  const updatedTheme = { updated: true };
-  let actualTheme;
-  const updateActualTheme = theme => {
-    actualTheme = theme;
+  const theme = { themed: true };
+  const update = { updated: true };
+  let actual;
+  const updateActual = x => {
+    actual = x;
   };
+  const expected = update;
 
   const ComponentWithTheme = withTheme(ThemePropInterceptor);
-  const broadcast = createBroadcast(initTheme);
+  const broadcast = createBroadcast(theme);
 
   mount(
     <Pure>
-      <ComponentWithTheme intercept={updateActualTheme} />
+      <ComponentWithTheme intercept={updateActual} />
     </Pure>,
     mountOptions(broadcast),
   );
 
-  t.deepEqual(
-    actualTheme,
-    initTheme,
-    `withTheme(Comp) should receive initial theme even through PureComponent`,
-  );
-
-  broadcast.publish(updatedTheme);
+  broadcast.publish(update);
 
   t.deepEqual(
-    actualTheme,
-    updatedTheme,
+    actual,
+    expected,
     `withTheme(Comp) should receive theme updates even through PureComponent`,
   );
 });
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,5 +1,6 @@
-import React, { PureComponent } from 'react';
+import React, { Component, PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import channel from '../channel';
 
 export class Pure extends PureComponent {
   static propTypes = {
@@ -15,3 +16,35 @@ export class Pure extends PureComponent {
 }
 
 export const Comp = props => <div {...props} />;
+
+export const getContextTypes = C => C.contextTypes;
+export const getChannel = C => Object.keys(getContextTypes(C))[0];
+
+export class ThemePropInterceptor extends Component {
+  static propTypes = {
+    intercept: PropTypes.func.isRequired,
+    theme: PropTypes.object.isRequired,
+  };
+  constructor(props) {
+    super(props);
+    this.props.intercept(props.theme);
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps) {
+      this.props.intercept(nextProps.theme);
+    }
+  }
+  // eslint-disable-next-line
+  render() {
+    return <div />;
+  }
+}
+
+export const mountOptions = broadcast => ({
+  childContextTypes: {
+    [channel]: PropTypes.func.isRequired,
+  },
+  context: {
+    [channel]: broadcast.subscribe,
+  },
+});
