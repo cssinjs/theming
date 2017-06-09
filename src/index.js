52113a09d8a7c9cf96a0ae5849de4c9ce32ea091
Vladimir Starkov <iamstarkov@gmail.com>
2017-06-10 01:20:01 +0200

    refactor: convert sources from cjs into esm

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-const createThemeProvider = require('./create-theme-provider');
-const createWithTheme = require('./create-with-theme');
-const channel = require('./channel');
+import createThemeProvider from './create-theme-provider';
+import createWithTheme from './create-with-theme';
+import defaultChannel from './channel';
 
-module.exports = {
-  channel,
-  withTheme: createWithTheme(),
-  ThemeProvider: createThemeProvider(),
-  createTheming: (customChannel = channel) => ({
+export const channel = defaultChannel;
+export const withTheme = createWithTheme();
+export const ThemeProvider = createThemeProvider();
+export function createTheming(customChannel = defaultChannel) {
+  return {
     channel: customChannel,
     withTheme: createWithTheme(customChannel),
     ThemeProvider: createThemeProvider(customChannel),
-  }),
+  };
+}
+
+export default {
+  channel: defaultChannel,
+  withTheme,
+  ThemeProvider,
+  createTheming,
 };
