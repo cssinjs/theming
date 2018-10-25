95fcac90a6080d44e50b8496c72b3f960a8fb946
Henri Beck <henribeck.dev@gmail.com>
2018-10-25 21:47:34 +0200

    Added support for innerRef and forwarding this ref to the next component

--- src/create-with-theme.js
+++ src/create-with-theme.js
@@ -5,27 +5,34 @@ import { type Context } from 'create-react-context';
 import hoist from 'hoist-non-react-statics';
 import getDisplayName from 'react-display-name';
 
+type Options = { forwardInnerRef?: boolean };
 type OuterPropsType<InnerProps, InnerComponent, Theme> = $Diff<InnerProps, { theme: Theme }> & {
   theme?: Theme,
-  innerRef?:(instance: ElementRef<InnerComponent> | null) => void
+  innerRef?: (instance: ElementRef<InnerComponent> | null) => void
 };
+type InnerPropsType<Theme> = { theme: Theme };
 
 export default function createWithTheme<Theme: {}>(context: Context<Theme>) {
-  function hoc<
-    InnerProps: { theme: Theme },
+  return function hoc<
+    InnerProps: InnerPropsType<Theme>,
     InnerComponent: ComponentType<InnerProps>,
     OuterProps: OuterPropsType<InnerProps, InnerComponent, Theme>,
-  >(Component: InnerComponent): ComponentType<OuterProps> {
+  >(
+    Component: InnerComponent,
+    { forwardInnerRef = false }: Options = {},
+  ): ComponentType<OuterProps> {
     function withTheme(props: OuterProps) {
-      const { innerRef, ...rest } = props;
+      // $FlowFixMe
+      const { innerRef, ...otherProps } = props;
+
+      otherProps[forwardInnerRef ? 'innerRef' : 'ref'] = innerRef;
 
       return (
         <context.Consumer>
           {theme => (
             <Component
-              ref={innerRef}
               theme={theme}
-              {...rest}
+              {...otherProps}
             />
           )}
         </context.Consumer>
@@ -37,7 +44,5 @@ export default function createWithTheme<Theme: {}>(context: Context<Theme>) {
     hoist(withTheme, Component);
 
     return withTheme;
-  }
-
-  return hoc;
+  };
 }
