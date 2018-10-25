db352c56b49f57c148ff14235eafe457b0d6fbc7
Henri Beck <henribeck.dev@gmail.com>
2018-10-25 21:55:07 +0200

    Added test for innerRef and forwarding the innerRef

--- src/create-with-theme.test.js
+++ src/create-with-theme.test.js
@@ -7,7 +7,19 @@ import TestRenderer from 'react-test-renderer';
 import createWithTheme from './create-with-theme';
 
 // eslint-disable-next-line no-unused-vars
-const Comp = (props: { theme: {} }) => null;
+const FunctionalComponent = (props: { theme: {} }) => null;
+
+class ClassComponent extends React.Component<{ theme: {} }> {
+  static displayName = 'foo';
+
+  static someSpecialStatic = 'bar';
+
+  inner = true;
+
+  render() {
+    return null;
+  }
+}
 
 test('createWithTheme\'s type', (t) => {
   t.true(typeof createWithTheme === 'function', 'createWithTheme should be a function');
@@ -23,56 +35,79 @@ test('createWithTheme\'s result is function on its own', (t) => {
 test('should pass the default value of the context', (t) => {
   const theme = {};
   const context = createReactContext(theme);
-  const WithTheme = createWithTheme(context)(Comp);
+  const WithTheme = createWithTheme(context)(FunctionalComponent);
   const { root } = TestRenderer.create(<WithTheme />);
 
-  t.true(root.findByType(Comp).props.theme === theme);
+  t.true(root.findByType(FunctionalComponent).props.theme === theme);
 });
 
 
 test('should pass the value of the Provider', (t) => {
   const theme = { test: 'test' };
   const context = createReactContext(theme);
-  const WithTheme = createWithTheme(context)(Comp);
+  const WithTheme = createWithTheme(context)(FunctionalComponent);
   const { root } = TestRenderer.create((
     <context.Provider value={theme}>
       <WithTheme />
     </context.Provider>
   ));
 
-  t.true(root.findByType(Comp).props.theme === theme);
+  t.true(root.findByType(FunctionalComponent).props.theme === theme);
 });
 
 test('should allow overriding the prop from the outer props', (t) => {
   const theme = {};
   const otherTheme = {};
   const context = createReactContext(theme);
-  const WithTheme = createWithTheme(context)(Comp);
+  const WithTheme = createWithTheme(context)(FunctionalComponent);
   const { root } = TestRenderer.create((
     <WithTheme theme={otherTheme} />
   ));
 
-  t.true(root.findByType(Comp).props.theme === otherTheme);
+  t.true(root.findByType(FunctionalComponent).props.theme === otherTheme);
+});
+
+test('innerRef should set the ref prop on the wrapped component', (t) => {
+  const context = createReactContext({});
+  const withTheme = createWithTheme(context);
+  let refComp = null;
+  const innerRef = (comp) => {
+    refComp = comp;
+  };
+  const WithTheme = withTheme(ClassComponent);
+
+  TestRenderer.create((
+    <WithTheme innerRef={innerRef} />
+  ));
+
+  t.deepEqual(refComp.inner, true);
+});
+
+test('should forward the innerRef to the wrapped component when forwardInnerRef is true', (t) => {
+  const context = createReactContext({});
+  const WithTheme = createWithTheme(context)(FunctionalComponent, { forwardInnerRef: true });
+  const innerRef = () => null;
+  const { root } = TestRenderer.create((
+    <WithTheme innerRef={innerRef} />
+  ));
+
+  t.true(root.findByType(FunctionalComponent).props.innerRef === innerRef);
 });
 
 test('withTheme(Comp) hoists non-react static class properties', (t) => {
   const context = createReactContext({});
   const withTheme = createWithTheme(context);
-  class ExampleComponent extends React.Component<{ theme: {} }> {
-    static displayName = 'foo';
+  const WithTheme = withTheme(ClassComponent);
 
-    static someSpecialStatic = 'bar';
-  }
-  const ComponentWithTheme = withTheme(ExampleComponent);
   t.deepEqual(
-    ComponentWithTheme.displayName,
+    WithTheme.displayName,
     'WithTheme(foo)',
     'withTheme(Comp) should not hoist react static properties',
   );
   t.deepEqual(
     // $FlowFixMe: Need to find a better way to type the hoist-non-react-statics
-    ComponentWithTheme.someSpecialStatic,
-    ExampleComponent.someSpecialStatic,
+    WithTheme.someSpecialStatic,
+    ClassComponent.someSpecialStatic,
     'withTheme(Comp) should hoist non-react static properties',
   );
 });
