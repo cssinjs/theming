2a5e0a67c77342e7ee8f6e19b5f4b566ac165a5b
Henri Beck <henribeck.dev@gmail.com>
2019-02-07 13:03:59 +0100

    Use named function

--- src/create-use-theme.js
+++ src/create-use-theme.js
@@ -5,11 +5,13 @@ import warning from 'tiny-warning';
 import isObject from './is-object';
 
 export default function createUseTheme<Theme>(context: Context<Theme>) {
-  return () => {
+  const useTheme = () => {
     const theme = React.useContext(context);
 
     warning(isObject(theme), '[theming] Please use useTheme only with the ThemeProvider');
 
     return theme;
   };
+
+  return useTheme;
 }
